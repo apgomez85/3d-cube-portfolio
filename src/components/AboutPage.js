56b9f2880e41edf70d6e54ba05a42b5159cccdfd
Adrian <apgomez85@gmail.com>
2019-12-30 09:07:53 -0800

    about me adjustments

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,15 +1,28 @@
 import React from "react";
 
+const style = {
+  display: "flex",
+  color: "#6A6C6E",
+  fontFamily: "ostrich-sans-light",
+  lineHeight: "1",
+  fontSize: "2.5rem",
+  marginTop: "60px",
+  flexDirection: "column",
+  maxWidth: "50%",
+  margin: "auto",
+  marginLeft: "80px"
+};
+
 export default function AboutPage() {
   return (
-    <div>
+    <div style={style}>
       <h1>About Me</h1>
       <p>
         I'm Adrian Gomez, a Web Developer based in Southern California. I have
         found my passion in web development and design. I love tech and problem
-        solving in all aspects. Right now, I specialize in the MERN stack and
-        all the bits and bytes in between, but will gladly explore other
-        avenues. Similar to the tech world, I am always in development.
+        solving in all aspects. I specialize in the MERN stack and all the bits
+        and bytes in between, but will gladly explore other avenues. Similar to
+        the tech world, I am always in development.
       </p>
     </div>
   );
