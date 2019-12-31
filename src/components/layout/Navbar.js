37faf6e5d201929acaf4871ffbce86725959a979
Adrian <apgomez85@gmail.com>
2019-12-31 12:41:49 -0800

    font size

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -22,7 +22,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import CubeLayout from "./CubeLayout";
 
-const drawerWidth = 240;
+const drawerWidth = 230;
 
 const useStyles = makeStyles(theme => ({
   root: {
