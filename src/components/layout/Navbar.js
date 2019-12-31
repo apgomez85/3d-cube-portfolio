af755ee0675fb59d111bd21e4527ddc563518468
Adrian <apgomez85@gmail.com>
2019-12-31 12:53:19 -0800

    font size

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -87,7 +87,7 @@ function ResponsiveDrawer(props) {
   };
 
   const drawer = (
-    <div style={{ maxHeight: "100%" }}>
+    <div style={{ maxWidth: "100%" }}>
       <img
         src={require("../../images/profile_pic.jpg")}
         alt="profile_pic"
