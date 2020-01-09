5f75ada7b2bfbdbca59912719cd98b8aad68ad75
Adrian <apgomez85@gmail.com>
2020-01-09 13:42:47 -0800

    resume changes

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -6,7 +6,7 @@ export default function ResumePage() {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="https://drive.google.com/open?id=1XbmAJOBEgqbnIwgXXRB2FMUpD_yQ8fbR"
+        href="https://drive.google.com/file/d/1jaw72Q5VC_qDkSIWbOQraTDcEthNzfjd/view"
         download
       >
         <button
